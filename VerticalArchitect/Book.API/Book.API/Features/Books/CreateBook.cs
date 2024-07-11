562a3299c43c9cf9abfe50cbbcd10f9196032e0b
m0nem <chessmonem@gmail.com>
2024-07-11 13:09:08 +0330

    Added feature: Create Book

---
+++ VerticalArchitect/Book.API/Book.API/Features/Books/CreateBook.cs
@@ -0,0 +1,35 @@
+﻿using Book.API.Data;
+using MediatR;
+
+namespace Book.API.Features.Books
+{
+    public static class CreateBook
+    {
+        public static void AddEndpoint(this IEndpointRouteBuilder app) 
+        {
+            app.MapPost("/api/books", async (CreateBookCommand request, ISender sender) =>
+            {
+                var bookId = await sender.Send(request);
+                return Results.Ok(bookId);
+            });
+        }
+    }
+
+    public record CreateBookCommand(API.Entities.Book Book):IRequest<int>;
+
+    internal class Handler : IRequestHandler<CreateBookCommand, int>
+    {
+        private readonly AppDbContext _appDbContext;
+        public Handler(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public  Task<int> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+        {
+            _appDbContext.books.Add(request.Book);
+            _appDbContext.SaveChanges();
+            return Task.FromResult(request.Book.ID);
+        }
+    }
+
+}
--- VerticalArchitect/Book.API/Book.API/Program.cs
+++ VerticalArchitect/Book.API/Book.API/Program.cs
@@ -2,6 +2,7 @@ using Book.API.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using FluentValidation;
+using Book.API.Features.Books;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,31 +17,9 @@ builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+CreateBook.AddEndpoint(app);
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
